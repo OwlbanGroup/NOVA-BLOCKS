3d11e02df4e0b4b1c20c0aa06e9812f834ee24b8
Sean Broome <bsean4890@gmail.com>
2025-03-19 00:21:00 +0000

    UPDATE

--- nova-blocks/backend/index.js
+++ nova-blocks/backend/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 5000;
+
+// Middleware for logging requests
+app.use(morgan('combined'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Middleware for CORS support
+app.use(cors());
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 // Basic route
 app.get('/', (req, res) => {
